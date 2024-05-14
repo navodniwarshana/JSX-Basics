5ca8cfe7429bbf333f4e6646be66827d93712cfa
NavodNiwarshana <n.navodniwarshana@gmail.com>
2024-05-14 19:47:33 +0530

    useState (react hooks)
    alert and number counter

--- vite-project/src/Subpages/Contact.jsx
+++ vite-project/src/Subpages/Contact.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderContent from '../pages/HeaderContent/HeaderContent'
 import Bodycontent from '../pages/BodyContent/Bodycontent'
 import Footercontent from '../pages/FooterContent/Footercontent'
 
 
 function Contact() {
+    
+    const styles_dev= //styling
+      {
+        fontSize:'2rem',color:'green',backgroundColor:'black',
+        width:'4rem',
+      }
+
+    //use state
+    ///let num=2;
+    const [num,setnum]=useState(0)
+    function handleClick(){
+        setnum(5);
+        //alert(num)
+    }
+    //--------------------------------
+    const [count,setcount]=useState(0)
+
+    const  incrfunc = () =>{
+        setcount(count +1)
+    }
+     
+    const dicrfunc = () =>{
+        setcount(count -1)
+    } 
+
   return (
     <>
         <HeaderContent/>
@@ -12,6 +37,14 @@ function Contact() {
         <Bodycontent>
 
             <h2>Contact Page</h2>
+            <h2>Use State</h2>
+            <button className='btn' onClick={handleClick}>Click Me</button>
+            <p>{num}</p><br />
+
+            <button style={styles_dev} onClick={dicrfunc}> - </button>
+            <button style={styles_dev} onClick={incrfunc}> + </button>
+            
+            <h3 >{count}</h3>
 
         </Bodycontent>
 
