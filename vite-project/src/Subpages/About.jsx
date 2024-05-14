9a619a8f4a87764342122a11baacf90d320f0f86
NavodNiwarshana <n.navodniwarshana@gmail.com>
2024-05-14 20:51:50 +0530

    And operation , Ternary operators

--- vite-project/src/Subpages/About.jsx
+++ vite-project/src/Subpages/About.jsx
@@ -2,13 +2,37 @@ import React from 'react'
 import HeaderContent from '../pages/HeaderContent/HeaderContent'
 import Bodycontent from '../pages/BodyContent/Bodycontent'
 import Footercontent from '../pages/FooterContent/Footercontent'
+import LoggedIn from './LoggedIn';
 
 function About() {
+
+  const loggedin=true;//false
+  const names =["Jane","Doe","jhone"];
+  const namescount=names.length;
+
+  const namearr =["amal","kamal","bimal"];
+  const namescount2=namearr.length;
+
   return (
     <>
       <HeaderContent/>
       <Bodycontent>
           <h2>About Page</h2>
+          {loggedin && <LoggedIn/> }
+          {!loggedin && <h2> Logged Out</h2>}
+
+          {namescount > 0 && names.map((value) =>  //use and operation
+              {
+                return <h2>{value}</h2>
+              }
+            )}
+          {namescount ==0 && <p>None Provided</p>}
+          
+          {/*use Ternary operators*/}
+          {namescount2 > 0 ? (namearr.map((name2)=>{
+            return <h3>{name2}</h3>
+          })) : (<p>None Provided</p>)}
+
       </Bodycontent>
       <Footercontent/>
     </>
---
+++ vite-project/src/Subpages/LoggedIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoggedIn() {
+  return (
+    <h2 style={{background:'green',}}>Logged In</h2>
+  )
+}
+
+export default LoggedIn
