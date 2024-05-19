c99e15654ad4268b918b7ba14902bed22d53b768
NavodNiwarshana <n.navodniwarshana@gmail.com>
2024-05-20 03:06:57 +0530

    Infinite Image Carousel / Slider
    Infinite :- use effect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,19 +2,54 @@ import './App.css'
 import HeaderContent from './pages/HeaderContent/HeaderContent'
 import Bodycontent from './pages/BodyContent/Bodycontent'
 import Footercontent from './pages/FooterContent/Footercontent'
-import Comp1 from './Componants/Comp1'
+import { data } from './data'
+import { useEffect, useState } from 'react'
 
 
 function App() {
 
+  const [activeImg,setActiveImg] = useState(0);
+  const handlePrev = () =>{
+    //alert("prev img")
+    if(activeImg <=0){
+      setActiveImg(data.length - 1)
+    }else{
+      setActiveImg(activeImg -1 )
+    }
+  }
+
+  useEffect (() =>{
+
+    let timer=setTimeout(() => {
+      handleNext();
+    },3000);
+
+    return() => {
+      clearTimeout(timer);
+    }
+
+  },[activeImg])
+
+  const handleNext = () =>{
+    //alert("Next img")
+    setActiveImg((activeImg +1) % data.length)
+  }
   return (
     <>
       <HeaderContent/>
       <Bodycontent>
-          <h2>Home Page</h2>
-      <Comp1/>
           
-          
+      <div className="carousal">
+        <button onClick={handlePrev}>Prev</button>
+        {
+          data.map((item,i)=>{
+            return(
+            <img className={activeImg === i ? 'img':"hide" }src={item.url} alt={item.alt} key={item.id}/>
+          )})
+        }
+        <button onClick={handleNext}>Next</button>
+      </div>
+
       </Bodycontent>
       <Footercontent/>
       
---
+++ vite-project/src/data.js
@@ -0,0 +1,12 @@
+export const data=[ 
+    {id:1, url:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    alt:"img 1"},
+    {id:2, url:"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    alt:"img 2"},
+    {id:3, url:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    alt:"img 3"},
+    {id:4, url:"https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    alt:"img 4"},
+    {id:5, url:"https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    alt:"img 5"}
+  ]
